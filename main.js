0e5bed7882eb133418b79465abeadf36bd53d449
高見知英 <chie@onpu-tamago.net>
2023-10-20 22:03:04 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+function main(){
+  const playlist = getPlaylists();
+  const videos = getRecentVideos();
+  Array.from(videos).forEach((v) => {
+    const p = findPlaylistForVideo(v, playlist);
+    if(p){
+      console.log(`${v.snippet.title} find by ${p.snippet.title}`);
+    }else{
+      console.log(`${v.snippet.title} no playlist`);
+    }
+  });
+}
+
+/**
+ * 直近20件の動画を取得する。
+ * @return {Youtube_v3.Youtube.V3.Schema.SearchResult[]} 直近の動画情報を含む配列。
+ */
+function getRecentVideos() {
+  return YouTube.Search.list(["snippet"], {
+    forMine: true,
+    maxResults: 20,
+    type:[ "video" ]
+  }).items;
+}
+
+/**
+ * プレイリストを取得する。
+ * @return {Youtube_v3.Youtube.V3.Schema.Playlist[]} すべてのプレイリストの情報を含む配列。
+ */
+function getPlaylists(){
+  let result = [];
+  let rests = NaN;
+  let token = "";
+  while(isNaN(rests) || rests > 0){
+    const pl = YouTube.Playlists.list(["snippet"], {
+      mine: true,
+      maxResults: 50,
+      pageToken: token
+    });
+    if(isNaN(rests)){
+      rests = pl.pageInfo.totalResults;
+    }
+    rests -= pl.pageInfo.resultsPerPage
+    token = pl.nextPageToken;
+    result = [...result, ...pl.items];
+  }
+  return result;
+}
+
+/**
+ * 動画の情報より。該当するプレイリストを取得する。
+ * @param {Youtube_v3.Youtube.V3.Schema.SearchResult} 動画の情報。
+ * @param {Youtube_v3.Youtube.V3.Schema.Playlist[]} すべてのプレイリストの情報を含む配列。
+ * @return {Youtube_v3.Youtube.V3.Schema.Playlist|null} 該当するプレイリスト。プレイリストが見つからない場合はnull。
+ */
+function findPlaylistForVideo(video, playlists){
+  let result = null;
+  if(video.snippet.title.includes("：")){
+    const title = video.snippet.title.split("：")[0];
+    const item = Array.from(playlists).find((list) => list.snippet.title == title);
+    if(item) result = item;
+  }
+  return result;
+}
