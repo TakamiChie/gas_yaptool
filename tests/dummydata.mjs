9cb905f18eeb0b69ca119c35096c75e1fe91fd54
高見知英 <chie@onpu-tamago.net>
2024-05-29 18:50:23 +0900

    簡単なテストコードを追加

---
+++ tests/dummydata.mjs
@@ -0,0 +1,92 @@
+/*
+ * ダミーデータ配列
+ * {Youtube_v3.Youtube.V3.Schema.SearchResult} 動画データ
+ */
+const searchResult = [];
+
+/*
+ * ダミーデータ配列
+ * {Youtube_v3.Youtube.V3.Schema.Playlist} プレイリストデータ
+ */
+const playLists = [];
+
+function addSearchResultData(title, description) {
+  const id = searchResult.length + 1;
+  searchResult.push({
+    kind: "youtube#searchResult",
+    etag: "Etag",
+    id: {
+      kind: "youtube#videoId",
+      videoId: `VIDEO_ID_${id}`
+    },
+    snippet: {
+      publishedAt: `PUBLISHED_AT_${id}`,
+      channelId: `CHANNEL_ID_${id}`,
+      title: title,
+      description: description,
+      thumbnails: {
+        default: {
+          url: `THUMBNAIL_URL_${id}-1`,
+          width: 120,
+          height: 90
+        },
+        medium: {
+          url: `THUMBNAIL_URL_${id}-2`,
+          width: 320,
+          height: 180
+        },
+        high: {
+          url: `THUMBNAIL_URL_${id}-3`,
+          width: 640,
+          height: 480
+        }
+      }
+    }
+  });
+}
+
+function addPlaylistData(title, description="説明文") {
+  const id = playLists.length + 1;
+  playLists.push({
+    kind: "youtube#playlist",
+    etag: "Etag",
+    id: {
+      kind: "youtube#playlistId",
+      playlistId: `PLAYLIST_ID_${id}`
+    },
+    snippet: {
+      publishedAt: `PUBLISHED_AT_${id}`,
+      channelId: `CHANNEL_ID_${id}`,
+      title: title,
+      description: description,
+      thumbnails: {
+        default: {
+          url: `THUMBNAIL_URL_${id}-1`,
+          width: 120,
+          height: 90
+        },
+        medium: {
+          url: `THUMBNAIL_URL_${id}-2`,
+          width: 320,
+          height: 180
+        },
+        high: {
+          url: `THUMBNAIL_URL_${id}-3`,
+          width: 640,
+          height: 480
+        }
+      }
+    }
+  });
+}
+
+addSearchResultData("プレイリストあり：プレイリスト指定あり動画", "説明文");
+addSearchResultData("プレイリストなし：プレイリスト指定あり動画", "説明文");
+addSearchResultData("プレイリスト指定なし動画", "説明文");
+addSearchResultData("ハッシュタグ指定あり動画", "説明文#プレイリストあり");
+addSearchResultData("ハッシュタグ指定あり動画", "説明文#プレイリストなし");
+addSearchResultData("ハッシュタグ複数指定あり動画", "説明文#プレイリストあり #プレイリスト2");
+addSearchResultData("プレイリストあり：ハッシュタグ指定あり動画", "説明文#プレイリスト2");
+addPlaylistData("プレイリストあり");
+addPlaylistData("プレイリスト2");
+module.exports = {searchResult, playLists};
---
+++ tests/test_findPlaylistsForVideo.test.js
@@ -0,0 +1,43 @@
+const {findPlaylistsForVideo} = require("../src/main");
+const {searchResult, playLists} = require("./dummydata");
+
+describe("正常系", () => {
+  it("動画に：でプレイリスト指定、該当のプレイリストあり", () => {
+    expect(findPlaylistsForVideo(searchResult[0], playLists).length).toBeGreaterThan(0);
+  });
+
+  it("動画に：でプレイリスト指定、該当のプレイリストなし", () => {
+    expect(findPlaylistsForVideo(searchResult[1], playLists).length).toBe(0);
+  });
+
+  it("動画にプレイリスト指定なし", () => {
+    expect(findPlaylistsForVideo(searchResult[2], playLists).length).toBe(0);
+  });
+
+  it("動画にハッシュタグでプレイリスト指定、該当のプレイリストあり", () => {
+    expect(findPlaylistsForVideo(searchResult[3], playLists).length).toBeGreaterThan(0);
+  });
+
+  it("動画にハッシュタグでプレイリスト指定、該当のプレイリストなし", () => {
+    expect(findPlaylistsForVideo(searchResult[4], playLists).length).toBe(0);
+  });
+
+  it("動画にハッシュタグでプレイリスト指定、該当のプレイリストが複数存在", () => {
+    expect(findPlaylistsForVideo(searchResult[5], playLists).length).toBe(2);
+  });
+
+  it("動画に：とハッシュタグでプレイリスト指定、該当のプレイリストが一つずつ存在", () => {
+    expect(findPlaylistsForVideo(searchResult[6], playLists).length).toBe(2);
+  });
+});
+
+
+describe("異常系", () => {
+  it("動画なし", () => {
+    expect(findPlaylistsForVideo(undefined, playLists).length).toBeUndefined();
+  });
+
+  it("プレイリストなし", () => {
+    expect(findPlaylistsForVideo(searchResult[0], []).length).toBe(0);
+  });
+});
